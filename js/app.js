0f43fdda20f57e936a84cad0fa06090c6aea974e
Albert Byiringiro <albert.byiringiro@proton.me>
2024-03-14 06:57:27 +0200

    Add Style

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let generator = document.querySelector(".joke-generator");
+let setup = document.querySelector(".joke-setup");
+let delivery = document.querySelector(".joke-delivery");
+
+generator.addEventListener("click", function () {
+    
+})
