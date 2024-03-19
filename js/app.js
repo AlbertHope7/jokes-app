425ac54ab9e329b3ae5aed23d8711f250274c06e
Albert Byiringiro <albert.byiringiro@proton.me>
2024-03-19 18:40:10 +0200

    initial css

--- js/app.js
+++
@@ -1,7 +0,0 @@
-let generator = document.querySelector(".joke-generator");
-let setup = document.querySelector(".joke-setup");
-let delivery = document.querySelector(".joke-delivery");
-
-generator.addEventListener("click", function () {
-    
-})
